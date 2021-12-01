900476ed16ecf790424a3f4b60d8342780b6c043
tiannian <dtiannian@gmail.com>
2021-12-01 09:50:54 +0800

    add basic logic for bridge

--- contracts/BridgeBSC.sol
+++ contracts/BridgeBSC.sol
@@ -1,29 +1,44 @@
-contract PledgerBridgeBSC {
+import './ERC20Safe.sol'
+
+contract PledgerBridgeBSC is ERC20Safe {
     address keeper_owner;
+    address plgr;
+
+    event DepositPLGR(address owner, uint256 amount, uint time);
+
+    event WithdrawPLGR(address owner, uint256 amount);
 
     // User call this function on BSC to deposit PLGR.
-    function deposit_plgr(bytes calldata data) external {
+    function deposit_plgr(address owner, uint256 amount) external {
+        lockERC20(plgr, owner, address(this), amount);
 
+        // Store user info.
+
+        emit DepositPLGR(owner, amount, now)
     }
 
     // User call this function on BSC to widthdraw PLGR.
-    function widthdraw_plgr(bytes calldata data) external {
+    function widthdraw_plgr(address owner, uint256 amount) external {
+        releaseERC20(plgr, owner, amount);
 
+        // Desc user info.
+
+        emit WithdrawPLGR(owner, amount);
     }
 
     // Chainbridge call this function on BSC
-    function deposit_mplgr(bytes calldata data) external {
+    function deposit_mplgr(bytes data) external {
 
     }
 
     // Keepers service call this funtion to get when and how to pass bridge.
-    function checkUpkeep(bytes calldata checkData) public view returns(bool, bytes memory) {
+    function checkUpkeep(bytes checkData) public view returns(bool, bytes memory) {
         address wallet = abi.decode(checkData, (address));
         return (wallet.balance < 1 ether, bytes(""));
     }
 
     // Execute keeper.
-    function performUpkeep(bytes calldata performData) external {
+    function performUpkeep(bytes performData) external {
         address[] memory wallets = abi.decode(performData, (address[]));
         for (uint256 i = 0; i < wallets.length; i++) {
             payable(wallets[i]).transfer(1 ether);
