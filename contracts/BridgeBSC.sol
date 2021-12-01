a8297b4b898261f7f5cc9d54a363579741f73eda
tiannian <dtiannian@gmail.com>
2021-12-01 21:26:39 +0800

    add ETH part from PLGR -> MPLGR

--- contracts/BridgeBSC.sol
+++ contracts/BridgeBSC.sol
@@ -1,8 +1,20 @@
-import './ERC20Safe.sol'
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.6.12;
+
+import './ERC20Safe.sol';
 
 contract PledgerBridgeBSC is ERC20Safe {
-    address keeper_owner;
-    address plgr;
+    address public owner;
+
+    address public plgr_address;
+
+    address public bridge_address;
+
+    constructor(address _plgr_address, address _bridge_address) public {
+        owner = msg.sender;
+        plgr_address = _plgr_address;
+        bridge_address = _bridge_address;
+    }
 
 
 
@@ -14,41 +26,40 @@ contract PledgerBridgeBSC is ERC20Safe {
     event WithdrawPLGR(address owner, uint256 amount);
 
     // User call this function on BSC to deposit PLGR.
-    function deposit_plgr(address owner, uint256 amount) external {
-        lockERC20(plgr, owner, address(this), amount);
+    function deposit_plgr(address _owner, uint256 amount) external {
+        lockERC20(plgr_address, _owner, address(this), amount);
 
         // Store user info.
 
-        emit DepositPLGR(owner, amount, now)
+        emit DepositPLGR(_owner, amount, now);
     }
 
     // User call this function on BSC to widthdraw PLGR.
-    function widthdraw_plgr(address owner, uint256 amount) external {
-        releaseERC20(plgr, owner, amount);
+    function widthdraw_plgr(address _owner, uint256 amount) external {
+        releaseERC20(plgr_address, _owner, amount);
 
         // Desc user info.
 
-        emit WithdrawPLGR(owner, amount);
+        emit WithdrawPLGR(_owner, amount);
     }
 
     // Chainbridge call this function on BSC
-    function deposit_mplgr(bytes data) external {
+    function deposit_mplgr(bytes calldata data) external {
         uint256      lenMetadata;
         bytes memory metadata;
 
         lenMetadata = abi.decode(data, (uint256));
         metadata = bytes(data[32:32 + lenMetadata]);
-
     }
 
     // Keepers service call this funtion to get when and how to pass bridge.
-    function checkUpkeep(bytes checkData) public view returns(bool, bytes memory) {
+    function checkUpkeep(bytes calldata checkData) public view returns(bool, bytes memory) {
         address wallet = abi.decode(checkData, (address));
         return (wallet.balance < 1 ether, bytes(""));
     }
 
     // Execute keeper.
-    function performUpkeep(bytes performData) external {
+    function performUpkeep(bytes calldata performData) external {
         address[] memory wallets = abi.decode(performData, (address[]));
         for (uint256 i = 0; i < wallets.length; i++) {
             payable(wallets[i]).transfer(1 ether);
--- contracts/BridgeETH.sol
+++ contracts/BridgeETH.sol
@@ -1,13 +1,72 @@
-contract PledgerBridgeBSC {
-    address keeper_owner;
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.6.12;
 
-    // User call this function on ETH to widthdraw MPLGR.
-    function widthdraw_mplgr(bytes calldata data) external {
+import './ERC20Safe.sol';
+import 'solidity-bytes-utils/contracts/BytesLib.sol';
 
+contract PledgerBridgeETH is ERC20Safe {
+    using BytesLib for bytes;
+
+    // chainbridge ERC20Handler address.
+    address public bridge_address;
+
+    // admin address;
+    address public owner;
+
+    // MPLGR ERC20 token address
+    address public mplgr_address;
+    
+    // Store mplgr amounts by address.
+    mapping(address => uint256) mplgr_amounts;
+
+    event WithdrawMPLGR(address recipient, uint256 amount);
+
+    constructor(address _bridge_address, address _mplgr_address) public {
+        owner = msg.sender;
+        bridge_address = _bridge_address;
+        mplgr_address = _mplgr_address;
     }
 
     // Chainbridge call this function on ETH
-    function deposit_mplgr_rule(bytes calldata data) external {
+    function deposit_mplgr_bridge(bytes calldata data) external {
+        require(msg.sender == bridge_address, "widthdraw_mplgr only called by bridge");
 
+        uint256 amountCount;
+        bytes memory mplgr_amounts_bytes;
+
+        amountCount = abi.decode(data, (uint256));
+        mplgr_amounts_bytes = bytes(data[32:32 + amountCount * ( 20 + 32) ]);
+
+        address addr;
+        uint256 amount;
+
+        uint offset_begin = 0;
+        uint offset_middle = 20;
+
+        for (uint i = 0; i != amountCount; i ++) {
+            offset_begin = i * (20 + 32);
+            offset_middle = (i + 1) * 20;
+
+            addr = mplgr_amounts_bytes.toAddress(offset_begin);
+            amount = mplgr_amounts_bytes.toUint256(offset_middle);
+
+            mplgr_amounts[addr] = amount;
+        }
     }
+
+    // User call this function on ETH to widthdraw MPLGR.
+    function widthdraw_mplgr(uint256 amount) external {
+        uint256 value = mplgr_amounts[msg.sender];
+
+        require(value >= amount, "You have no enough MPLGR");
+
+        mplgr_amounts[msg.sender] = value - amount;
+
+        releaseERC20(mplgr_address, msg.sender, amount);
+
+        emit WithdrawMPLGR(msg.sender, amount);
+    }
+
+    // Call bridge
+    // function deposit_mplgr(uint256 amount) external {}
 }
