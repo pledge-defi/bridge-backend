5e3ec0444bbec6c99ac38dfc12e06b81272b8197
tiannian <dtiannian@gmail.com>
2021-12-02 15:39:06 +0800

    add test

---
+++ test/PLGR.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PLGR is ERC20 {
+    constructor(uint256 initialSupply) ERC20("PLGR", "PLGR") {
+        _mint(msg.sender, initialSupply);
+    }
+}
