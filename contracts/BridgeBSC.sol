3354428ef47d86917e0969d68a4873efd99765bb
tiannian <dtiannian@gmail.com>
2021-12-03 08:56:14 +0800

    add upkeep function

--- contracts/BridgeBSC.sol
+++ contracts/BridgeBSC.sol
@@ -15,14 +15,23 @@ contract PledgerBridgeBSC is ERC20Safe {
 
     address public handler_address;
 
+    uint256 public wait_time;
+
     struct LockedPLGRTx {
         address owner;
         uint256 amount;
+    }
+
+    struct LockInfo {
+        bytes32 txid;
         uint256 time;
     }
 
     mapping (address => uint256) public plgr_amounts;
+
     mapping (bytes32 => LockedPLGRTx) public locked_plgr_tx;
+
+    LockInfo[] public locked_infos;
 
     uint256 plgr_lock_nonce = 0;
 
@@ -37,8 +46,13 @@ contract PledgerBridgeBSC is ERC20Safe {
         handler_address = _handler_address;
     }
 
+    function admin_update_configure(uint256 _wait_time) external {
+        require(msg.sender == owner, "Admin only called by owner");
 
-    function admin_update_bridge(address _bridge_address, address _handler_address) public {
+        wait_time = _wait_time;
+    }
+
+    function admin_update_bridge(address _bridge_address, address _handler_address) external {
         require(msg.sender == owner, "Admin only called by owner");
 
         bridge_address = _bridge_address;
@@ -51,13 +65,14 @@ contract PledgerBridgeBSC is ERC20Safe {
         bytes32 txid = keccak256(abi.encode(_owner, amount, now, plgr_lock_nonce));
         plgr_lock_nonce ++;
 
-        LockedPLGRTx memory locked = locked_plgr_tx[txid];
-        locked.owner = _owner;
-        locked.amount = amount;
-        locked.time = now;
+        locked_plgr_tx[txid].owner = _owner;
+        locked_plgr_tx[txid].amount = amount;
 
         lockERC20(plgr_address, _owner, address(this), amount);
-        locked_plgr_tx[txid] = locked;
+
+        LockInfo memory lock_info = LockInfo (txid, now);
+
+        locked_infos.push(lock_info);
 
         emit DepositPLGR(txid, _owner, amount, now);
     }
@@ -83,5 +98,23 @@ contract PledgerBridgeBSC is ERC20Safe {
         uint256 amount = data.toUint256(20);
 
         plgr_amounts[addr] += amount / 3;
+    }
+
+    function check_upkeep() view public returns (int256) {
+        for(uint i = 0; i != locked_infos.length; i ++) {
+            if (locked_infos[i].time + wait_time > now) {
+                return int256(i) - 1;
+            }
+        }
+    }
+
+    function execute_upkeep(int256 index) external returns(bool result, bytes memory data) {
+        if (index < 0) {
+            return (false, bytes(""));
+        } else {
+            for (uint i = 0; i <= uint256(index); i ++) {
+
+            }
+        }
     }
 }
