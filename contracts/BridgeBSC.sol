df78e2dd5eb8b9978a679606fbe0613cbec5eed8
Kevin <chensiqi_scdy@163.com>
2021-12-30 21:28:49 -0800

    implement unkeep.

--- contracts/BridgeBSC.sol
+++ contracts/BridgeBSC.sol
@@ -21,6 +21,10 @@ contract PledgerBridgeBSC is ERC20Safe {
 
     uint256 public wait_time;
 
+    uint256 public release_count = 0;
+    uint256 public price = 1;
+    uint256 public total_pledge = 0;
+
     // Arguments for chainbridge.
     uint8 public cb_ddid;
     bytes32 public cb_rid;
@@ -38,6 +42,7 @@ contract PledgerBridgeBSC is ERC20Safe {
     mapping (address => uint256) public plgr_amounts;
 
     mapping (bytes32 => LockedPLGRTx) public locked_plgr_tx;
+    mapping (bytes32 => LockedPLGRTx) public can_release;
 
     LockInfo[] public locked_infos;
 
@@ -112,6 +117,25 @@ contract PledgerBridgeBSC is ERC20Safe {
         uint256 amount = data.toUint256(20);
 
         plgr_amounts[addr] += amount / 3;
+        total_pledge += amount / 3;
+    }
+
+    function get_base(uint256 _count) view public returns(uint256) {
+        uint256 base = 0;
+        uint256 n = _count / 12 + 1; // 3 months = 12 weeks
+        if (n <= 4) { // 1~12 months
+            base = n * 100000 * 10 ** 18;
+        } else if (n > 4 && n <= 8) { // 12~24 months
+            base = 2000000 * 10 ** 18;
+        } else if (n > 8 && n <= 12) { // 24~36 months
+            base = 4000000 * 10 ** 18;
+        } else if (n > 12 && n <= 16) { // 36~48 months
+            base = 10000000 * 10 ** 18;
+        } else { // 48~ months
+            base = 20000000 * 10 ** 18;
+        }
+
+        return base;
     }
 
     function check_upkeep() view public returns (int256) {
@@ -119,42 +143,79 @@ contract PledgerBridgeBSC is ERC20Safe {
             return -1;
         }
 
+        mapping(bytes32 => uint256) txid_release_amount;
+        uint256[] index_txid;
         for(uint i = 0; i != locked_infos.length; i ++) {
             if (locked_infos[i].time + wait_time > now) {
                 return int256(i) - 1;
+            } else {
+                bytes32 txid = locked_infos[i].txid;
+                txid_release_amount[txid] = locked_plgr_tx[txid].amount;
+                index_txid.push(txid);
             }
         }
+
+        // to do: get price from oracle.
+        uint256 x = 0;
+        if (price < 1) {
+            x = 1;
+        } else if (price >= 1 && price <= 125) {
+            // to do: x is the cubic root of price.
+            x = 1;
+        } else {
+            x = 5;
+        }
+
+        uint256 base = get_base(release_count);
+        require(base > 0, "ERROR: base is zero");
+        uint256 amount = base * x / 4;
+
+        for (uint i = 0; i< index_txid.length; i++) {
+            bytes32 txid = index_txid[i];
+            uint256 amount = txid_release_amount[txid] * amount / total_pledge;
+
+            require(locked_plgr_tx[txid].amount >= amount, "ERROR: Insufficient remaining pledge");
+            locked_plgr_tx[txid].amount -= amount;
+
+            can_release[txid].owner = locked_plgr_tx[txid].owner;
+            can_release[txid] = amount;
+        }
+
+        return 0;
     }
 
     function execute_upkeep(int256 index) external {
         uint256 count = uint256(index) + 1;
 
         bytes memory rdata = abi.encode(count);
-
+        uint256 total_release = 0;
         if (index >= 0) {
             for (uint i = 0; i <= uint256(index); i ++) {
-                bytes32 txid = locked_infos[i].txid;
+                bytes32 txid = can_release[i].txid;
 
-                bytes memory addr = abi.encodePacked(locked_plgr_tx[txid].owner);
+                bytes memory addr = abi.encodePacked(can_release[txid].owner);
                 rdata = rdata.concat(addr);
 
-                bytes memory amount = abi.encode(locked_plgr_tx[txid].amount);
-
+                bytes memory amount = abi.encode(can_release[txid].amount);
                 rdata = rdata.concat(amount);
 
-                delete locked_plgr_tx[txid];
-                delete locked_infos[i];
+                total_release += can_release[txid].amount;
+
+                delete can_release[txid];
+                if (locked_plgr_tx[txid].amount < 1*10**15) { // amount < 0.001
+                    delete locked_plgr_tx[txid];
+                    delete locked_infos[i];
+                }
             }
 
             bytes memory args_bytes = abi.encode(rdata);
-
             bytes memory length = abi.encode(args_bytes.length);
-
             bytes memory args = length.concat(args_bytes);
-
             IBridge bridge = IBridge(bridge_address);
-
             bridge.deposit(cb_ddid, cb_rid, args);
+
+            release_count += 1;
+            total_release -= total_release;
         }
     }
 }
