8683a33bda47467611e047e183a525d93bd1fa76
Kevin <chensiqi_scdy@163.com>
2022-01-04 00:24:30 -0800

    fix

--- contracts/BridgeBSC.sol
+++ contracts/BridgeBSC.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity ^0.6.12;
+pragma solidity ^0.8.0;
 
 import './ERC20Safe.sol';
 import './ABDKMathQuad.sol';
 import 'solidity-bytes-utils/contracts/BytesLib.sol';
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 interface IBridge {
     function deposit(uint8 destinationDomainID, bytes32 resourceID, bytes calldata data) external payable;
@@ -47,6 +47,8 @@ contract PledgerBridgeBSC is ERC20Safe {
 
     mapping (bytes32 => LockedPLGRTx) public locked_plgr_tx;
     mapping (bytes32 => LockedPLGRTx) public can_release;
+    mapping(bytes32 => uint256) txid_release_amount;
+    bytes32[] index_txid;
 
     LockInfo[] public locked_infos;
 
@@ -100,7 +102,7 @@ contract PledgerBridgeBSC is ERC20Safe {
     // User call this function on BSC to deposit PLGR.
     function deposit_plgr(address _owner, uint256 amount) external returns(bytes32) {
 
-        bytes32 txid = keccak256(abi.encode(_owner, amount, now, plgr_lock_nonce));
+        bytes32 txid = keccak256(abi.encode(_owner, amount, block.timestamp, plgr_lock_nonce));
         plgr_lock_nonce ++;
 
         locked_plgr_tx[txid].owner = _owner;
@@ -108,11 +110,11 @@ contract PledgerBridgeBSC is ERC20Safe {
 
         lockERC20(plgr_address, _owner, address(this), amount);
 
-        LockInfo memory lock_info = LockInfo (txid, now);
+        LockInfo memory lock_info = LockInfo (txid, block.timestamp);
 
         locked_infos.push(lock_info);
 
-        emit DepositPLGR(txid, _owner, amount, now);
+        emit DepositPLGR(txid, _owner, amount, block.timestamp);
 
         return txid;
     }
@@ -148,53 +150,50 @@ contract PledgerBridgeBSC is ERC20Safe {
             return -1;
         }
 
-        mapping(bytes32 => uint256) txid_release_amount;
-        uint256[] index_txid;
+        return 0;
+    }
+
+    function execute_upkeep(int256 index) external {
         for(uint i = 0; i != locked_infos.length; i ++) {
-            if (locked_infos[i].time + wait_time > now) {
-                return int256(i) - 1;
-            } else {
+            if (locked_infos[i].time + wait_time <= block.timestamp) {
                 bytes32 txid = locked_infos[i].txid;
                 txid_release_amount[txid] = locked_plgr_tx[txid].amount;
                 index_txid.push(txid);
             }
         }
 
+        uint256 count = index_txid.length;
+
         require(base > 0, "base is zero");
         require(x > 0, "x is zero");
         // base * x / 4
         uint total_release = ABDKMathQuad.toUInt(
-                                 ABDKMathQuad.div(
-                                     ABDKMathQuad.mul(ABDKMathQuad.fromUInt(base), ABDKMathQuad.fromUInt(x)),
-                                     ABDKMathQuad.fromUInt(4)));
+            ABDKMathQuad.div(
+                ABDKMathQuad.mul(ABDKMathQuad.fromUInt(base), ABDKMathQuad.fromUInt(x)),
+                ABDKMathQuad.fromUInt(4)));
 
 
         for (uint i = 0; i < index_txid.length; i++) {
             bytes32 txid = index_txid[i];
             // (personal amount / total pledge) * total release
             uint256 amount = ABDKMathQuad.toUInt(
-                                 ABDKMathQuad.div(
-                                     ABDKMathQuad.mul(ABDKMathQuad.fromUInt(txid_release_amount[txid]), ABDKMathQuad.fromUInt(total_release)),
-                                     ABDKMathQuad.fromUInt(total_pledge*1000)));
+                ABDKMathQuad.div(
+                    ABDKMathQuad.mul(ABDKMathQuad.fromUInt(txid_release_amount[txid]), ABDKMathQuad.fromUInt(total_release)),
+                    ABDKMathQuad.fromUInt(total_pledge*1000)));
 
             require(locked_plgr_tx[txid].amount >= amount, "Insufficient remaining pledge");
             locked_plgr_tx[txid].amount = locked_plgr_tx[txid].amount.sub(amount);
 
             can_release[txid].owner = locked_plgr_tx[txid].owner;
-            can_release[txid] = amount;
+            can_release[txid].amount = amount;
         }
 
-        return 0;
-    }
-
-    function execute_upkeep(int256 index) external {
-        uint256 count = uint256(index) + 1;
 
         bytes memory rdata = abi.encode(count);
-        uint256 total_release = 0;
+        //uint256 total_release = 0;
         if (index >= 0) {
             for (uint i = 0; i <= uint256(index); i ++) {
-                bytes32 txid = can_release[i].txid;
+                bytes32 txid = index_txid[i];
 
                 bytes memory addr = abi.encodePacked(can_release[txid].owner);
                 rdata = rdata.concat(addr);
@@ -205,6 +204,7 @@ contract PledgerBridgeBSC is ERC20Safe {
                 total_release += can_release[txid].amount;
 
                 delete can_release[txid];
+                delete index_txid[i];
                 if (locked_plgr_tx[txid].amount < 1*10**15) { // amount < 0.001
                     delete locked_plgr_tx[txid];
                     delete locked_infos[i];
