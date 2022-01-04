1e8366e7c91948ff1ad698b654c8ed0ee0e3ddf1
zTgx <747674262@qq.com>
2022-01-05 00:05:14 +0800

    fix deploy eth

--- migrations/3_deploy_bridgeETH.js
+++ migrations/3_deploy_bridgeETH.js
@@ -1,9 +1,9 @@
 const PledgerBridgeETH = artifacts.require("PledgerBridgeETH");
 
 module.exports = function (deployer) {
-  const mplgr_address = "0x550ea36E01Ada1d6dEd5fEfeA30E70aCA9275051";
-  const bridge_address = "0x0C4DD68A014B58e0DDD590Bb014b0f7E71D2DaF4";
-  const handler_address = "0x361F5886C42c631A241b1fE29f6BbfEf800B021f";
+  const mplgr_address = "0xb7d51b050f2072C2855B21579bB61955Cb484a97";
+  const bridge_address = "0x909882A2a12157148331957baC08eDbc06C1f4F2";
+  const handler_address = "0x3629cCFf769D3A29886fdE4be3777287AAF31337";
   const ddid = 0;
   const rid = "0x000000000000000000000000000000c76ebe4a02bbc34786d860b355f5a5ce10";
 
